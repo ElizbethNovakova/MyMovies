985788dd1d543ba0a0a8c7e5f7b2790e5bd88b49
ElizbethNovakova <lizochka1211@gmail.com>
2019-12-14 15:41:40 +0200

    Created recycle view, and we can see the list of films

---
+++ app/src/main/java/com/example/mymovies/MovieAdapter.java
@@ -0,0 +1,63 @@
+package com.example.mymovies;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.mymovies.data.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
+
+    private ArrayList<Movie> movies;
+    public MovieAdapter(){
+        movies = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_item, viewGroup, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder movieViewHolder, int i) {
+        Movie movie = movies.get(i);
+        Picasso.get().load(movie.getPosterPath()).into(movieViewHolder.imageViewSmallPoster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView imageViewSmallPoster;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
+        }
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    public void addMovies(ArrayList<Movie> movies){
+        this.movies.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+}
