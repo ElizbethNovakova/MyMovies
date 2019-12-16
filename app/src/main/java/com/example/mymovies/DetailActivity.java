fc08aaaf5803cea6b1c47a8aa3aeda4924862b12
ElizbethNovakova <lizochka1211@gmail.com>
2019-12-16 22:49:21 +0200

    Created list of favourite movies and debbuged some errors

--- app/src/main/java/com/example/mymovies/DetailActivity.java
+++ app/src/main/java/com/example/mymovies/DetailActivity.java
@@ -4,16 +4,19 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mymovies.data.FavouriteMovie;
 import com.example.mymovies.data.MainViewModel;
 import com.example.mymovies.data.Movie;
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
-
+    private ImageView imageViewAddToFavourite;
     private ImageView imageViewBigPoster;
     private TextView textviewTitle;
     private TextView textViewOriginalTitle;
@@ -24,6 +27,8 @@ public class DetailActivity extends AppCompatActivity {
 
     private int id;
     private MainViewModel viewModel;
+    private Movie movie;
+    private FavouriteMovie favouriteMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,9 @@ public class DetailActivity extends AppCompatActivity {
         textviewTitle = findViewById(R.id.textViewTitle);
         textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
         textViewRating = findViewById(R.id.textViewRating);
-        textViewReleaseDate = findViewById(R.id.textViewLableReleaseDate);
+        textViewReleaseDate = findViewById(R.id.textViewReleaseDate);
         textViewOverview = findViewById(R.id.textViewOverview);
+        imageViewAddToFavourite = findViewById(R.id.imageViewAddToFavourite);
         Intent intent = getIntent();
         if( intent != null && intent.hasExtra("id")){
             id =intent.getIntExtra("id",-1);
@@ -43,13 +49,33 @@ public class DetailActivity extends AppCompatActivity {
             finish();
         }
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        Movie movie = viewModel.getMovieById(id);
+        movie = viewModel.getMovieById(id);
         Picasso.get().load(movie.getBigPosterPath()).into(imageViewBigPoster);
         textviewTitle.setText(movie.getTitle());
         textViewOriginalTitle.setText(movie.getOriginalTitle());
         textViewOverview.setText(movie.getOverview());
         textViewReleaseDate.setText(movie.getReleaseDate());
         textViewRating.setText(Double.toString(movie.getVoteAverage()));
+        setFavourite();
+    }
 
+    public void onClickChangeFavourite(View view) {
+        if(favouriteMovie == null){
+            viewModel.insertFavouriteMovies(new FavouriteMovie(movie));
+            Toast.makeText(this, getString(R.string.add_to_favourite), Toast.LENGTH_SHORT).show();
+        } else {
+            viewModel.deleteFavouriteMovies(favouriteMovie);
+            Toast.makeText(this, getString(R.string.deleted_from_favourite), Toast.LENGTH_SHORT).show();
+        }
+        setFavourite();
+    }
+
+    private void setFavourite(){
+        favouriteMovie = viewModel.getFavouriteMovieById(id);
+        if(favouriteMovie == null){
+            imageViewAddToFavourite.setImageResource(R.drawable.star2);
+        } else {
+            imageViewAddToFavourite.setImageResource(R.drawable.star);
+        }
     }
 }
